aecf4ee1bc70aacc16c757ee2010c2027481e341
Carmona Colin Jose Alb <al221411868@gmail.com>
2021-05-14 15:19:55 -0500

    UploadImage

--- pages/API/templates/update.vue
+++ pages/API/templates/update.vue
@@ -13,22 +13,21 @@
                   Template {{ $route.query.id }}
                 </div>
                 <div class="card-body">
-                  <form action="" v-for="item in data" :key="item.id">
-                    <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input type="text" class="form-control"  placeholder="Name Template" v-model="item.name" />
-                        
-                    </div>
-                    <div class="form-group">
+                    <form action="" :data='data' :key="data.id">
+                      <div class="form-group">
+                        <label for="name">Name: </label>
+                        <input type="text" class="form-control"  placeholder="Name Template" v-model="data.name"  />
+                      </div>
+                      <div class="form-group">
                         <label for="name">Content:</label>
                         <client-only placeholder="loading..." >
-                          <ckeditor-nuxt  :config="editorConfig"  v-model="item.content"/>
+                          <ckeditor-nuxt :config="editorConfig"  v-model="data.content" />
                         </client-only>
-                    </div>
-                    <div class="form-group">
-                      <button class="btn btn-warning id" type="submit" @click="updateTemplate" >Update Template...</button>
-                    </div>
-                  </form>
+                      </div>
+                      <div class="form-group">
+                        <button class="btn btn-warning" type="submit" @click="updateTemplate" >Update Template...</button>
+                      </div>
+                    </form>
                 </div>
               </div>
             </div>    
@@ -51,37 +50,48 @@ export default {
     head () {
       return {
         title: 'Diken - Dashboard',
+        
       }
     },
 
     data() {
         return{
             id: this.$route.query.id,
-            data: null,
+            data: [],
             form: {
               name: "",
               content: ""
+            },
+            editorConfig: {
+              simpleUpload: {
+                uploadUrl: 'http://127.0.0.1:8000/api/mailImage',
+                headers: {
+                  'Authorization':this.$auth.getToken('local'),
+                  'content-type': 'multipart/form-data'
+                }
+              }
             }
         }
-    },
-    mounted() {
-      $('.id').on('click',function(){
-        alert("Console")
-      })
     },
     methods:{
       
         async template(){
-            let template = await this.$axios.get('http://127.0.0.1:8000/api/template/'+this.id)
-            console.log(template)
+            const template = await this.$axios.get('http://127.0.0.1:8000/api/template/'+this.id)
+            //console.log(template.data.template)
             //this.data = template.data.template
+            //console.log(template.data.template)
+            this.data = template.data.template
+ 
+            
+            //console.log(this.$auth.strategy)
+            
+            
         },
         async updateTemplate(e){
           e.preventDefault();
-
-          let template = await this.$axios.put('http://127.0.0.1:8000/api/template/'+this.id, this.data[0])
+          let template = await this.$axios.put('http://127.0.0.1:8000/api/template/'+this.id, this.data)
           console.log(template)
-          //this.$router.push('/API/templates/');
+          this.$router.push('/API/templates/');
         }
     },
 
